4bb5644ada071b20387b8c627f4817a22efd04cf
Lucas Konstantin Baerenfaenger <18075314+typeduke@users.noreply.github.com>
2023-03-07 22:05:11 +0100

    Add tutorial-server-websockets

---
+++ tutorial-server-websockets/src/main/kotlin/com/typeduke/Application.kt
@@ -0,0 +1,13 @@
+package com.typeduke
+
+import io.ktor.server.application.*
+import com.typeduke.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSockets()
+    configureRouting()
+}
---
+++ tutorial-server-websockets/src/main/kotlin/com/typeduke/plugins/Sockets.kt
@@ -0,0 +1,42 @@
+package com.typeduke.plugins
+
+import Connection
+import io.ktor.server.websocket.*
+import java.time.Duration
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import io.ktor.websocket.*
+import java.util.Collections
+
+fun Application.configureSockets() {
+    install(WebSockets) {
+        pingPeriod = Duration.ofSeconds(15)
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+    routing {
+        val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
+        webSocket("/chat") {
+            println("Adding user!")
+            val thisConnection = Connection(this)
+            connections += thisConnection
+            try {
+                send("You are connected! There are ${connections.count()} users here.")
+                for (frame in incoming) {
+                    frame as? Frame.Text ?: continue
+                    val receivedText = frame.readText()
+                    val textWithUserName = "[${thisConnection.name}]: $receivedText"
+                    connections.forEach {
+                        it.session.send(textWithUserName)
+                    }
+                }
+            } catch (e: Exception) {
+                println(e.localizedMessage)
+            } finally {
+                println("Removing $thisConnection!")
+                connections -= thisConnection
+            }
+        }
+    }
+}
